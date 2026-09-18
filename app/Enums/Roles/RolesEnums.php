4b8b51b7e532fdf8b8498376cf3afc4b9791840c
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2026-09-18 02:16:16 +0100

    setup workspace v:16

--- app/Enums/Roles/RolesEnums.php
+++ app/Enums/Roles/RolesEnums.php
@@ -5,21 +5,23 @@ namespace App\Enums\Roles;
 enum RolesEnums: string
 {
     case SUPERADMIN = 'SUPERADMIN';
-    case CUSTOMER= 'CUSTOMER';
+    case CUSTOMER = 'CUSTOMER';
+    case INSTRUCTOR = 'INSTRUCTOR';
 
     // extra helper to allow for greater customization of displayed values, without disclosing the name/value data directly
     public function label(): string
     {
         return match ($this) {
-            self::SUPERADMIN => 'Administrateur',
+            self::SUPERADMIN => 'Administrator',
             self::CUSTOMER => 'Customer',
+            self::INSTRUCTOR => 'Instructor',
         };
     }
 
     public static function options()
     {
         return collect(self::cases())
-            ->mapWithKeys(fn (self $type) => [
+            ->mapWithKeys(fn(self $type) => [
                 $type->value => $type->label(),
             ])
             ->toArray();
